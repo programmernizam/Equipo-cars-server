84abcf8dbbd8e5eb1f900eedd59727e3d618ab1c
programmerNizam <mdnizam12109@gmail.com>
2022-05-23 01:42:11 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.s7i3i.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const partsCollection = client.db("equipoCars").collection("parts");
+    // Add parts method
+    app.post('/parts', async(req, res)=>{
+        
+    })
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
+app.listen(port, () => {
+  console.log("Worked");
+});
