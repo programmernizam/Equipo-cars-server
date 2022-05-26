6edc8462416357199e4e7232990fcf20e3577b44
programmerNizam <mdnizam12109@gmail.com>
2022-05-26 18:37:30 +0600

    Install heroku

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
@@ -36,6 +37,18 @@ async function run() {
     const reviewsCollection = client.db("equipoCars").collection("reviews");
     const ordersCollection = client.db("equipoCars").collection("orders");
     const userCollection = client.db("equipoCars").collection("users");
+    // Payment Method
+    app.post("/create-payment-intent", verifyJwt, async (req, res) => {
+      const orders = req.body;
+      const total = orders.total;
+      const amount = total * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
     // Add parts method
     app.post("/parts", async (req, res) => {
       const parts = req.body;
@@ -98,7 +111,7 @@ async function run() {
       const orders = await ordersCollection.insertOne(item);
       res.send(orders);
     });
-    app.get("/order", async (req, res) => {
+    app.get("/all-order", async (req, res) => {
       const query = {};
       const orders = await ordersCollection.find(query).toArray();
       res.send(orders);
@@ -114,12 +127,13 @@ async function run() {
         return res.status(403).send({ message: "forbidden access" });
       }
     });
-    app.get('/orders/:id', verifyJwt, async(req, res) =>{
+    app.get("/orders/:id", verifyJwt, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const orders = await ordersCollection.findOne(query);
-      res.send(order);
-    })
+      res.send(orders);
+    });
+
     // Delete orders
     app.delete("/orders/:id", async (req, res) => {
       const id = req.params.id;
@@ -167,7 +181,7 @@ async function run() {
       const token = jwt.sign(
         { email: email },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: "12h" }
+        { expiresIn: "1h" }
       );
       res.send({ result, token });
     });
