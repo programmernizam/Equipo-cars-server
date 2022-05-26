4e12cac28a413ff20dccc4b890857905d531efaf
programmerNizam <mdnizam12109@gmail.com>
2022-05-26 19:02:21 +0600

    Fix bug

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIP_SECRET_KEY);
 
 app.use(cors());
 app.use(express.json());
